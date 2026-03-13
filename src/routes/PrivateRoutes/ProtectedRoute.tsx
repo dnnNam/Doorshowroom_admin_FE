0f1c97a5288772b14b28c1c027cf45e81434b579
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2026-03-13 13:26:00 +0700

    #18 done check auth

---
+++ src/routes/PrivateRoutes/ProtectedRoute.tsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from "react-router";
+import { useAuth } from "@/hooks/useAuth";
+import path from "@/constants/path";
+export default function ProtectedRoute() {
+  const { data: user, isLoading } = useAuth();
+  if (isLoading) return <div>Loading...</div>;
+  const isAuthenticated = !!user;
+  return isAuthenticated ? <Outlet /> : <Navigate to={path.login} replace />;
+}
